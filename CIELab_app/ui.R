ffd7398615436b2e190f0a58afbc55946fc11bad
mikelkou <mikaela.koutrouli@cpr.ku.dk>
2021-07-16 16:19:42 +0200

    Update ui.R

--- CIELab_app/ui.R
+++ CIELab_app/ui.R
@@ -8,7 +8,7 @@
 #
 
 # Define UI for application that draws a histogram
-source('~/Documents/Documents – SUN1012692/GitHub/CIELAB/CIELab_app/global.R', local = TRUE)
+source('global.R', local = TRUE)
 options(shiny.maxRequestSize = 100*1024^2)
 # shinyUI(
 
