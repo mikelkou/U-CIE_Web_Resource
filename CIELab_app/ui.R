b95066f9655b31a8c73005d1dca208942320e2b2
mikelkou <mikaela.koutrouli@cpr.ku.dk>
2021-09-13 10:45:53 +0200

    Update ui.R

--- CIELab_app/ui.R
+++ CIELab_app/ui.R
@@ -366,8 +366,8 @@ dashboardPage(skin = "purple",
                             tags$head(tags$script(src = "intro.js")),
                             tags$head(tags$script(src = "introbutton.js")),
                             tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "intro.css")),
-                            tags$head(tags$link(rel = "icon", type="image/x-icon", href = "favicon.ico"),
-                                      tags$link(rel = "shortcut icon", type="image/x-icon", href = "favicon.ico")),
+                            tags$head(tags$link(rel = "icon", type="image/x-icon", href = "www/favicon.ico"),
+                                      tags$link(rel = "shortcut icon", type="image/x-icon", href = "www/favicon.ico")),
 
                             class = "dropdown")
                     
